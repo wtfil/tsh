60f177f480743c028304c7f2c8c257d6c9438f6e
Evgen Filatov <evgen.filatov@gmail.com>
2014-12-07 14:07:04 +0000

    replaced script with tmux

--- tsh.sh
+++ tsh.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-FILE=/tmp/terminal-share-test
-ROOM_ID=`Date | md5`
+FILE=~/tmux.ha  
+ROOM_ID=`date | sha256sum | head -c 16`
 SERVER="https://glacial-oasis-4823.herokuapp.com"
 >$FILE
 
@@ -17,16 +17,17 @@ done
 
 prev=''
 while true; do
+    echo rere
+    tmux capture-pane -t 0 \; save-buffer -b 0 $FILE
 	content=`cat $FILE`
 	if [[ $content != $prev ]]; then
 		curl -d "$content" "$SERVER/rooms/$ROOM_ID" &> /dev/null
 		prev=$content;
 	fi;
-	sleep 0.1
-done &
+	sleep 1
+done
+echo $!
 
 trap "kill 0" exit INT TERM
 
 echo -e "\n\tStreaming started. Type \"exit\" to stop streaming:\n\t\033[37m\033[4m$SERVER/$ROOM_ID\033[0m\n"
-
-script -q -t 0.1 $FILE
